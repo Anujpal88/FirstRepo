eeb2e314d5f6ff541f1f97cac85e156b87bc7bf7
Anujpal88 <46216952+Anujpal88@users.noreply.github.com>
2018-12-28 14:31:25 +0530

    Anuj

--- src/test/java/Demo/demo/Demos.java
+++ src/test/java/Demo/demo/Demos.java
@@ -5,5 +5,6 @@ public class Demos
 	public static void main(String[] args) {
 		 System.out.println("New class created 28 dec...");
 		System.out.println("Update demo class 28 dec...");
+		System.out.println("Update demo class 28 dec...Updated by Anuj");
 	}
 }
